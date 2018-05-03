a7312f53110e9392858eff7cfba32631220e61f4
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-05-03 21:08:15 +0530

    minor change

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['setup',['setup',['../classstarlings.html#ae3237d3011a7d17127f7c5d3b7db4c8d',1,'starlings']]],
+  ['starlings',['starlings',['../classstarlings.html',1,'']]]
+];
